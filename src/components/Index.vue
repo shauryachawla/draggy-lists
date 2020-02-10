f6f399985eb114a15b6d5068b4ad9e4923a950c2
Shaurya Chawla <emorres25@gmail.com>
2020-02-10 09:48:16 +0530

    First commit

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">Hello {{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
